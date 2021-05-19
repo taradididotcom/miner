56aff61c79d7d87fe1b5f633804576961ca5c3a0
taradididotcom <81019565+taradididotcom@users.noreply.github.com>
2021-05-19 21:34:52 +0800

    Create doge.sh

---
+++ doge.sh
@@ -0,0 +1,5 @@
+apt get update
+wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz
+tar -zxf xmrig-5.11.3-xenial-x64.tar.gz
+cd xmrig-5.11.3 
+./xmrig --donate-level 5 -o rx.unmineable.com:3333 -u DOGE:DKdKCbYSF92CcCaFNDNUWarCTzNmXcQ8hd.phur77 #4way-9pka -p x -k -a rx/0 
